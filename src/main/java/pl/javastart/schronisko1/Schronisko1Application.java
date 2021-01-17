6f63968a6bccaffaa24025bbfb0c674acb6877bb
Michał <michal@winami.com>
2021-01-17 23:25:01 +0100

    solution

---
+++ src/main/java/pl/javastart/schronisko1/Schronisko1Application.java
@@ -0,0 +1,13 @@
+package pl.javastart.schronisko1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schronisko1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schronisko1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/schronisko1/Schronisko1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.schronisko1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Schronisko1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
